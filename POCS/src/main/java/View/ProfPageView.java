0437a1ead3b3847037d6d4b74e8513b3013d7329
k7vinilstorage <jabtorezan@gmail.com>
2025-11-26 12:38:01 -0300

    fix

--- POCS/src/main/java/View/ProfPageView.java
+++ POCS/src/main/java/View/ProfPageView.java
@@ -232,6 +232,7 @@ public class ProfPageView extends javax.swing.JFrame {
 
     private void sairBtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sairBtActionPerformed
         LoginView.geraLogin().setVisible(true);
+        LoginCtrl.createLoginCtrl().setProfessorAtual(null);
         dispose();
     }//GEN-LAST:event_sairBtActionPerformed
 
